fe6a79dc3b09437a92ea428e5e0886e82f10d40b
翁浩平 <wenghaoping@souche.com>
2018-06-22 18:12:40 +0800

    feat:修改

--- reamname.js
+++ reamname.js
@@ -1,28 +1,54 @@
 const fs = require('fs');
-fs.readdir('./bizhi', (err, files) => {
-        //err 为错误 , files 文件名列表包含文件夹与文件
-    if(err){
-        console.log('error:\n' + err);
-        return;
-    }
-    let arr = [];
-    let i = 0;
-    files.forEach((file) => {
-        fs.stat(`./bizhi/${file}`, (err2, stat) => {
-            if(err2){console.log(err2); return;}
-            if(stat.isDirectory()){
-                fs.readdir(`./bizhi/${file}`, (err3, files2) => {
-                    if(err3){console.log(err3); return;}
-                    // 读出所有的文件
-                    console.log('文件名:' + files2);
-                    files2.forEach((file2) => {
+
+// fs.readdir('./bizhi', (err, files) => {
+//         //err 为错误 , files 文件名列表包含文件夹与文件
+//     if(err){
+//         console.log('error:\n' + err);
+//         return;
+//     }
+//     let arr = [];
+//     let i = 0;
+//     files.forEach((file) => {
+//         fs.stat(`./bizhi/${file}`, (err2, stat) => {
+//             if(err2){console.log(err2); return;}
+//             if(stat.isDirectory()){
+//                 fs.readdir(`./bizhi/${file}`, (err3, files2) => {
+//                     if(err3){console.log(err3); return;}
+//                     // 读出所有的文件
+//                     console.log('文件名:' + files2);
+//                     files2.forEach((file2) => {
+//                         i++;
+//                         fs.rename(`./bizhi/${file}/${file2}`, `./bizhi2/${i}.jpg`, (err) => {
+//                             console.log(err);
+//                         })
+//                     })
+//                 });
+//             }
+//         });
+//     });
+// })
+
+
+let readdir = (path) => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        fs.readdir(path, (err, files) => {
+            if (err) { console.log(err); return; }
+            files.forEach((file) => {
+                fs.stat(`${path}/${file}`, (err2, stat) => {
+                    if(err2){console.log(err2); return;}
+                    if(stat.isDirectory()){
+                        readdir(`${path}/${file}`);
+                    } else {
                         i++;
-                        fs.rename(`./bizhi/${file}/${file2}`, `./bizhi2/${i}.jpg`, (err) => {
+                        console.log(`${path}/${file}`);
+                        fs.rename(`${path}/${file}`, `./bizhi2/${i}.jpg`, (err) => {
                             console.log(err);
                         })
-                    })
-                });
-            }
+                    }
+                })
+            });
         });
-    });
-})
+    })
+}
+readdir("./bizhi");
